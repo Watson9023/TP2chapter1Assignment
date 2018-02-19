be7d571703959c7d8b8c19498252cae291bedab6
Watson9023 <matunhirewendy@gmail.com>
2018-02-19 13:08:19 +0200

    Uploaded project to repo

---
+++ mavern_test2/src/main/java/com/watson/AddInteger.java
@@ -0,0 +1,9 @@
+package com.watson;
+
+public class AddInteger
+{
+    int sum(int int1,int int2){
+
+        return int1+int2;
+    }
+}
---
+++ mavern_test2/src/test/java/com/watson/AddIntegerTest.java
@@ -0,0 +1,16 @@
+package com.watson;
+
+import junit.framework.TestCase;
+
+public class AddIntegerTest extends TestCase {
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+    }
+
+    public void testSum() throws Exception {
+    }
+
+}
